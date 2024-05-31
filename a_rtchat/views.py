77a5b4e7edeb0b9d86bc1fec373d12ef00efd4d4
saurav <shresthsaurav8@gmail.com>
2024-05-31 19:30:02 +0545

    feat: display chat_msg in chat.html from db

--- a_rtchat/views.py
+++ a_rtchat/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import *
 
 
+@login_required
 def chat_view(request):
-    return render(request, 'a_rtchat/chat.html')
+    chat_group = get_object_or_404(ChatGroup, group_name = "PublicChat")
+    chat_messages = chat_group.chat_messages.all()[:30]
+
+    return render(request, 'a_rtchat/chat.html',{'chat_messages': chat_messages})
